e37890aafe3df64940a711ea85473cc5ed74fedd
CrisTo <toaquizacris@hotmail.com>
2020-07-25 22:02:30 -0500

    render data from nasa api

---
+++ src/components/App.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+
+const App = () => {
+  const [title, setTitle] = useState();
+  const [explanation, setExplanation] = useState();
+  const [url, setUrl] = useState();
+  const [date, setDate] = useState();
+
+  const fetchData = async () => {
+    const response = await fetch(
+      "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY"
+    );
+    const data = response.json();
+    const { title, explanation, url, date } = data;
+    setTitle(title);
+    setExplanation(explanation);
+    setUrl(url);
+    setDate(date);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Astronomy Picture of the Day</h1>
+      <h2>{title}</h2>
+      <img src={url} />
+      <p>{explanation}</p>
+      <span>{date}</span>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/__tests__/App.spec.js
@@ -0,0 +1,79 @@
+import React from "react";
+import { create, act } from "react-test-renderer";
+import App from "../App";
+
+global.fetch = jest.fn();
+
+describe("App", () => {
+  const data = {
+    date: "2020-07-25",
+    explanation: "some large explanation here",
+    title: "some title here",
+    url: "http://the-url-here",
+  };
+
+  const response = {
+    json: () => data,
+  };
+
+  beforeEach(() => {
+    fetch.mockClear();
+    fetch.mockResolvedValue(response);
+  });
+
+  it("should get picture of the day data from NASA api", () => {
+    act(() => {
+      create(<App />);
+    });
+
+    expect(fetch).toBeCalledTimes(1);
+  });
+
+  it("should render the title on a header tag", async () => {
+    let appComponent;
+    await act(async () => {
+      appComponent = create(<App />);
+    });
+    const root = appComponent.root;
+
+    const header = root.findByType("h2");
+
+    expect(header.props.children).toBe(data.title);
+  });
+
+  it("should render the url on a image tag", async () => {
+    let appComponent;
+    await act(async () => {
+      appComponent = create(<App />);
+    });
+    const root = appComponent.root;
+
+    const image = root.findByType("img");
+
+    expect(image.props.src).toBe(data.url);
+  });
+
+  it("should render the explanation on a paragraph tag", async () => {
+    let appComponent;
+    await act(async () => {
+      appComponent = create(<App />);
+    });
+    const root = appComponent.root;
+
+    const paragraph = root.findByType("p");
+
+    expect(paragraph.props.children).toBe(data.explanation);
+  });
+
+  it("should render the date on a span tag", async () => {
+    let appComponent;
+    await act(async () => {
+      appComponent = create(<App />);
+    });
+    const root = appComponent.root;
+
+    const span = root.findByType("span");
+
+    expect(span.props.children).toBe(data.date);
+  });
+});
